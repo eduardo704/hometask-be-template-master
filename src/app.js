d5a45f4ba64d75a703aac25fb88a851e5cbf9905
Eduardo Vargas <eduardo704@gmail.com>
2023-10-17 13:14:04 -0300

    fixed getting contracts id with different header id

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model')
+const { getProfile } = require('./middleware/getProfile')
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
@@ -11,11 +11,15 @@ app.set('models', sequelize.models)
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models')
+    const headerProfile = req.profile;
+    const { id } = req.params
+
+    if (headerProfile.id !== Number(id)) return res.status(403).end()
+
+    const contract = await Contract.findOne({ where: { id } })
+    if (!contract) return res.status(404).end()
+    res.json(req.profile)
 })
 module.exports = app;
